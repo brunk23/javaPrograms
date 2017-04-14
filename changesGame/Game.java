78d6309759386619599aef25ec326449be1263cb
Kevin Brunelle <brunellek@gmail.com>
2017-04-14 16:57:40 -0400

    Test exception throwing
    Still need to add "World.java" and build things up.  There's a lot
    of work to do here.

---
+++ changesGame/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+    public static void main(String[] args) {
+	GamePiece foo = new GamePiece();
+	
+	foo.setSize("large");
+	System.out.println(foo);
+	foo.setSize("SMAll");
+	System.out.println(foo);
+
+    }
+
+}
--- changesGame/GamePiece.java
+++ changesGame/GamePiece.java
@@ -11,19 +11,19 @@ public class GamePiece {
 
     void setSize(String description) {
 	if( description.equalsIgnoreCase("large") ) {
-	    size = true;
+	    isLarge = true;
 	} else {
-	    if( description.eqauls.IgnoreCase("small") ) {
-		size = false;
+	    if( description.equalsIgnoreCase("small") ) {
+		isLarge = false;
 	    } else {
 		// THROW ERROR
-		throw new IllegalArgumentException;
+		throw new IllegalArgumentException(description);
 	    }
 	}
     }
 
     boolean getSize() {
-	return size;
+	return isLarge;
     }
 
     int getColor() {
@@ -39,21 +39,21 @@ public class GamePiece {
     }
 
     // Will print a string description of the object.
-    String toString() {
+    public String toString() {
 	String description = new String("A ");
 	if( isLarge ) {
 	    description += "large ";
 	} else {
 	    description += "small ";
 	} 
-
+	return description;
     }
 
     // Returns the number of differences
     int compareTo(GamePiece other) {
 	int count = 0;
 
-	if( size != other.getSize() ) {
+	if( isLarge != other.getSize() ) {
 	    count++;
 	}
 	if( color != other.getColor() ) {
@@ -71,7 +71,6 @@ public class GamePiece {
 
 
     public GamePiece() {
-	
     }
 
 }
