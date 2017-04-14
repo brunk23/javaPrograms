3bb9cfb873c48a13281d8f1f056f6f89fdc1dd25
Kevin Brunelle <brunellek@gmail.com>
2017-04-14 11:31:06 -0400

    Start fleshing out GamePiece
    Build in definition of what this is.

--- changesGame/GamePiece.java
+++ changesGame/GamePiece.java
@@ -4,6 +4,63 @@
  */
 
 public class GamePiece {
-    
+    boolean isLarge;
+    int color;
+    boolean hole;
+    int shape;
+
+    void setSize(String description) {
+	if( description.equalsIgnoreCase("large") ) {
+	    size = true;
+	} else {
+	    if( description.eqauls.IgnoreCase("small") ) {
+		size = false;
+	    } else {
+		// THROW ERROR
+		throw new IllegalArgumentException;
+	    }
+	}
+    }
+
+    boolean getSize() {
+	return size;
+    }
+
+    int getColor() {
+	return color;
+    }
+
+    boolean hasHole() {
+	return hole;
+    }
+
+    int getShape() {
+	return shape;
+    }
+
+    // Returns the number of differences
+    int compareTo(GamePiece other) {
+	int count = 0;
+
+	if( size != other.getSize() ) {
+	    count++;
+	}
+	if( color != other.getColor() ) {
+	    count++;
+	}
+	if( hole != other.hasHole() ) {
+	    count++;
+	}
+	if( shape != other.getShape() ) {
+	    count++;
+	}
+
+	return count;
+    }
+
+
+    public GamePiece() {
+	
+    }
 
 }
