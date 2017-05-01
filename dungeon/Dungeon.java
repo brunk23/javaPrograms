9486a9338d1e9e942f6788f109d24a543c4de2ea
Kevin Brunelle <brunellek@gmail.com>
2017-05-01 13:47:07 -0400

    Small notes on the dungeon class

--- dungeon/Dungeon.java
+++ dungeon/Dungeon.java
@@ -1,8 +1,10 @@
 public class Dungeon {
     private Room[][] rooms;
 
+    // This will create the dungeon, populate the rooms,
+    // and place the player and door.
     public Dungeon(int size) {
-	rooms = new Room[size][size];
+	rooms = new Room[size+2][size+2];
     }
 
 }
